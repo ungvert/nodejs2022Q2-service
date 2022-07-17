923382f2e8bef6a026bdfe53a1764b4689b04fff
ungvert <pavel.chelyapin@gmail.com>
2022-07-17 14:16:29 +0300

    feat: openapi docs endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
+import { readFile } from 'fs/promises';
+import { dirname, join } from 'path';
+import { parse } from 'yaml';
 import { AppModule } from './app.module';
 
 const port = 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -13,6 +18,12 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const rootDirname = dirname(__dirname);
+  const DOC_API = await readFile(join(rootDirname, 'doc', 'api.yaml'), 'utf-8');
+  const document = parse(DOC_API);
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(port);
 }
 bootstrap();
@@ -21,4 +32,5 @@ console.log(`
   🚀  Server is running!
   🔉  Listening on port ${port}
   📭  Query at http://localhost:${port}
+  📄  OpenApi docs at http://localhost:${port}/doc
 `);
