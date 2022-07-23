2d885724723dc5929fddac1dffe50854ffda0f0f
ungvert <pavel.chelyapin@gmail.com>
2022-07-23 13:37:27 +0300

    feat: setup typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,21 @@ import { UsersModule } from './users/users.module';
 import { AlbumsModule } from './albums/albums.module';
 import { TracksModule } from './tracks/tracks.module';
 import { FavouritesModule } from './favourites/favourites.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     InMemoryDbModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
     UsersModule,
     ArtistsModule,
     AlbumsModule,
