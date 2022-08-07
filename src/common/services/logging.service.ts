a39b5f8b173542d2d68f3bbc5cb8ddc52693c57e
ungvert <pavel.chelyapin@gmail.com>
2022-08-07 20:12:32 +0300

    feat: Add listener and logging to uncaughtException and unhandledRejection events

--- src/common/services/logging.service.ts
+++ src/common/services/logging.service.ts
@@ -1,4 +1,26 @@
 import { ConsoleLogger, Injectable } from '@nestjs/common';
 
 @Injectable()
-export class LoggingService extends ConsoleLogger {}
+export class LoggingService extends ConsoleLogger {
+  constructor() {
+    super();
+
+    process.on('uncaughtException', (err, origin) => {
+      this.error(`Uncaught exception: ${err} ${origin}`);
+    });
+    process.on('unhandledRejection', (reason, promise) => {
+      this.error(`Unhandled rejection: ${reason} `);
+    });
+
+    /**
+     * You can test logging of exception handlers with this 2 uncommented lines:
+     */
+    // setTimeout(
+    //   () => Promise.reject(new Error('unhandledRejection test')),
+    //   5000,
+    // );
+    // setTimeout(() => {
+    //   throw new Error('uncaughtException test');
+    // }, 5000);
+  }
+}
